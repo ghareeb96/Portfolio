033d5feae3b53b49333b35d774881a62515f661c
Ghareeb Shehata <ghreeb_shehata@outlook.com>
2022-01-22 02:35:31 +0200

    Responsive updates

--- src/Sections/About/About.jsx
+++ src/Sections/About/About.jsx
@@ -23,10 +23,12 @@ const About = ({ setActiveSection }) => {
         <div className="section about-section" id="About" ref={aboutSection}>
             <div className="container">
                 <div className="about-content">
-                    <div className="details-section">
+                    <div className="grid-1">
                         <h4>Hello Friend ! </h4>
                         <h4>I am <span>Ghareeb Shehata</span></h4>
-                        <p>A MERN Stack web developer, Designing a nice-looking layout of a website and turning this design to code, also creating a server and a database and handling the connection between both frontend and backend with a readable, maintainable and well-structured lines of code. Producing a high performing websites.
+                    </div>
+                    <div className="grid-2">
+                        <p>A MERN Stack web developer, Designs a nice-looking layout of a website and turning this design to code, also creating a server and a database and handling the connection between both frontend and backend with a readable, maintainable and well-structured lines of code. Producing a high performing websites.
                         </p>
                         <p>
                             Always seeking for opportunity to get more knowledge of programming and design to enhance my own career. </p>
